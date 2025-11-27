a19666171640a5ac901b1919e5979685a98ccc7f
sem4rcet-spec <sem4.rcet@gmail.com>
2025-11-27 00:32:50 +0000

    conversion of Datatypes

---
+++ 01_basics/03-convertionOperation.js
@@ -0,0 +1,27 @@
+let score ="prerit"
+
+console.log(typeof score) 
+console.log(typeof(score))
+
+let valueInNumber = Number(score)
+console.log("valueInNumber", valueInNumber)
+console.log(typeof valueInNumber)
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1 , false => 0
+
+let isLoggedIn = "hello"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn)
+
+//1 => true; 0 => false
+// "" => false; "hello" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+// console.log(stringNumber)
+// console.log(typeof stringNumber)
+
