288069a4e5751538cf31ca1af51d61acfc3952f6
sem4rcet-spec <sem4.rcet@gmail.com>
2025-11-27 07:39:41 +0000

    dtyoe summary

--- 01_basics/03-convertionOperation.js
+++ 01_basics/03-convertionOperation.js
@@ -24,4 +24,3 @@ let someNumber = 33
 let stringNumber = String(someNumber)
 // console.log(stringNumber)
 // console.log(typeof stringNumber)
-
---
+++ 01_basics/04-datatypesSummary.js
@@ -0,0 +1,31 @@
+categorised on basis how data is stored and manipulated in memory
+
+// Primitive Data Types
+
+// 7 types :
+// 1. Number
+// 2. String
+// 3. Boolean
+// 4. Undefined
+// 5. Null
+// 6. BigInt
+// 7. Symbol
+
+// Non-Primitive Data Types(reference types):
+// 1. Object
+// 2. Array
+// 3. Function
+
+// js is a dynamically typed language
+const id = Symbol("123")
+const anotherId = Symbol("123")
+
+console.log(id === anotherId) // false
+
+const bigNumber = 2809128391823918239182391823n
+
+let myObj = {
+    name: "prerit",
+    age: 24,
+    isLoggedIn: false
+}
